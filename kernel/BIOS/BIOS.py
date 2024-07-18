35959cdb3aee7c61e126943f4368e759d3ea7cb6
webbrowser11 <terpstragraham@gmail.com>
2024-07-18 10:52:31 -0700

    Update BIOS.py

--- kernel/BIOS/BIOS.py
+++ kernel/BIOS/BIOS.py
@@ -1,1 +1,8 @@
-#no contents yet
+while True:
+ answer = input("exit?")
+ if answer == "no":
+   print("SkyOS v2.2 BIOS UPDATE")
+   print("this device is running SkyOS")
+   print("/kernel/bios")
+   print(" ^ bios is running here")
+  if answer == "yes":
