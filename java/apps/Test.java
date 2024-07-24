c01655cb89f603b3730e098b6fd5c28713044ce4
MicroboyLLC <103317426+MicroboyLLC@users.noreply.github.com>
2024-07-24 18:31:59 -0400

    Update Test.java

--- java/apps/Test.java
+++ java/apps/Test.java
@@ -1,6 +1,8 @@
 // This is a simple skyOS app.
 // You can modify this code to fit your needs.
 
+package com.example.test;
+
 class TestApp {
   public static void appMain() {
     System.out.println("Hello, World!");
