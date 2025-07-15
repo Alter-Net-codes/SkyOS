9077bd71ceac94990f017aa0df04766603886256
T_cat9000_2 <terpstragraham@gmail.com>
2025-07-15 15:05:24 -0700

    update

--- apps/virtualpet.py
+++ apps/virtualpet.py
@@ -86,23 +86,7 @@ def main():
             print("Goodbye!")
             print("See you later.")
             print("Saving and quitting...")
-
-            # Use absolute path based on this file's dir for kernel script
-            this_dir = os.path.dirname(os.path.abspath(__file__))
-            kernel_script = os.path.join(this_dir, '..', 'KERNEL', 'kernel.py')
-
-            if os.path.isfile(kernel_script):
-                try:
-                    subprocess.run([sys.executable, kernel_script], check=True)
-                except subprocess.CalledProcessError as e:
-                    print(f"Error executing the kernel script: {e}")
-                except Exception as e:
-                    print(f"An unexpected error occurred: {e}")
-            else:
-                print("Kernel Script Not found or has a defect. Please reinstall SkyOS or replace the kernel file.")
-
-            break
-
+            sys.exit()
         else:
             print("That is not a valid action!")
 
